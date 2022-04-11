c5496d7db4ac5a93dacc85428d718278ee530b7c
Admin <jadrejcic.a99@gmail.com>
2022-04-11 11:43:01 +0200

    add commits page

---
+++ src/views/Commits.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="commits">
+    <h1>Commits</h1>
+    <ul>
+      <li v-for="commit in commits" :key="commit.sha">
+        {{ commit.sha }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Commits",
+  data() {
+    return {
+      commits: [],
+    };
+  },
+  mounted() {
+    fetch("https://api.github.com/repos/vuejs/vue/commits")
+      .then((response) => response.json())
+      .then((data) => (this.commits = data));
+  },
+};
+</script>
